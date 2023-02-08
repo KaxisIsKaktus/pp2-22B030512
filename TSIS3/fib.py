cb6a742d2f3b5ed76c6e99709cdd014581f64979
kaxis <aamanzhol557@gmail.com>
2023-02-08 14:13:13 +0600

    week4

---
+++ TSIS3/fib.py
@@ -0,0 +1,15 @@
+#just testing what can i do with for, while or do-while loops
+
+x = int(input())
+a = 0
+b = 1
+c = b
+print(a)
+print(b)
+for x in range(x-2):
+    c = a + b
+    print(c)
+    a = b
+    b = c
+
+#yeah, its works
---
+++ TSIS3/zheti_ata.py
@@ -0,0 +1,8 @@
+#write a code to find ancestor of the kid
+#7 ata in other words
+print("neshinshi atandy bilgin keledi?")
+x = int(input())
+def anc(*h):
+    print("The", x, "ancestor is", h[-x])
+
+anc("lol", "kek", "cheburek", "hmm", "let", "me", "decide", "what", "happen")
