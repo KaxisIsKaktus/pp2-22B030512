833ae63bb04c27a102c149d1f66b46dcb9da6889
kaxis <aamanzhol557@gmail.com>
2023-03-06 00:27:03 +0600

    made in week7

---
+++ TSIS6/builtin-functions/5.py
@@ -0,0 +1,5 @@
+Tuple1 = [True , True , True , True , True ]
+Tuple2 = [False , True , True , True , True ]
+Tuple3 = [False , False , False , False , True ]
+Tuple4 = [False , False , False , False , False ]
+print(all(Tuple1))
